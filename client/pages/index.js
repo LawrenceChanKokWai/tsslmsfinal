c08e102311f8585fec3c6543ed6aa958f1a38896
Lawrence <68629118+LawrenceChanKokWai@users.noreply.github.com>
2021-11-04 17:10:59 +0800

    first commit

---
+++ client/pages/index.js
@@ -0,0 +1,13 @@
+const Index = () => {
+	return (
+		<>
+			<h1 className="jumbotron text-center bg-primary square">
+				TSS
+				<p>Learning Management System</p>
+			</h1>
+			<p>from next js...</p>
+		</>
+	);
+};
+
+export default Index;
